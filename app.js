fc8b93b61ea177c4f21a662ee86c2725b5c95130
2493787138 <2493787138@qq.com>
2023-07-17 18:51:17 +0800

    7.17

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fs = require('fs')
+const morgan = require('morgan')
+const cors=require('cors')
+const router=require('./router')
+const errorHandler=require('./middleware/error-handler')
+
+const app = express()
+const PORT=3000 //端口号
+
+//日志
+app.use(morgan('dev'))
+
+//配置解析表单请求体
+app.use(express.json())//json
+app.use(express.urlencoded())//x-www-form
+
+//跨域资源处理
+app.use(cors())
+
+//挂载路由
+app.use('/api',router)
+
+
+//在所有路由之后处理404
+app.use((req,res,next)=>{
+    res.status(404).send('404 找不到该路径')
+})
+
+//在所有中间件后配置错误处理中间件
+app.use(errorHandler())
+
+app.listen(PORT, () => {
+    console.log('server running')
+})
---
+++ config/dbconfig.js
@@ -0,0 +1,8 @@
+var dbconfig = {
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'root',
+    database:'expressProject' //所连接的数据库名
+}
+module.exports =dbconfig;
