e1c95d88a735c15ed65368d3e354c9f92c05a4ea
2493787138 <2493787138@qq.com>
2023-08-02 18:15:05 +0800

    8.2 chx(1)

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ var dbconfig = {
     host:'localhost',
     port:3306,
     user:'root',
-    password:'cx1037',
+    password:'root',
     database:'expressProject' //所连接的数据库名
 }
 module.exports =dbconfig;
